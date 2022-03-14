9801bc2f8ce777ac364e52b377ffe2c4582f95af
Noah Lundberg <nhlund@gmail.com>
2022-03-14 19:50:50 +0100

    finish backend

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler");
 
+const Item = require("../models/itemModel");
+
 // @desc       Get items
 // @route       GET /items
 // @access      Private
 const getItem = asyncHandler(async (req, res) => {
-    
+    const items = await Item.find();
 
-    res.status(200).json({message: "get items"})
+    res.status(200).json(items)
 })
 
 // @desc       set items
@@ -18,22 +20,43 @@ const setItem = asyncHandler(async (req, res) => {
         throw new Error("please add a text")
     }
 
+    const item = await Item.create({
+        text: req.body.text
+    })
 
-    res.status(200).json({message: "post items"})
+    res.status(200).json(item)
 })
 
 // @desc       Update items
 // @route       PUT /items/:id
 // @access      Private
 const updateItem = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `put items ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if(!item) {
+        res.status(400);
+        throw new Error("Item not found")
+    }
+
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+
+    res.status(200).json(updatedItem)
 })
 
 // @desc       Delete items
 // @route       DELETE /items/:id
 // @access      Private
 const deleteItem = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `delete items ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if(!item) {
+        res.status(400);
+        throw new Error("Item not found")
+    }
+
+    await item.remove();
+
+    res.status(200).json({id: req.params.id})
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 
-app.use("/items", require("./routes/itemRoutes"))
+app.use("/items", require("./routes/itemRoutes")); 
 
 app.use(errorHandler);
 
